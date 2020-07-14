102b39581cb9b935186dc9a5ee1d99fe58572d80
klfoulk16 <57010984+klfoulk16@users.noreply.github.com>
2020-07-13 21:36:32 -0400

    Added new html files and customized them
    create layout html template to base all other site pages off of,  customized html pages to reflect website's purpose

--- application.py
+++ application.py
@@ -1,8 +1,31 @@
 from flask import Flask, render_template
 
+# Configure application
 app = Flask(__name__)
+
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure debugger is enabled
+app.config["debug"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    """Buy shares of stock"""
+    return render_template('about.html')
